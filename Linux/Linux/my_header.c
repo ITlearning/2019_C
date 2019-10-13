b457b3859dc08b5c172729ebe5289231976d3519
ITlearning <yo7504@naver.com>
2019-10-13 23:17:02 +0900

    Linux_Edit

--- Linux/Linux/my_header.c
+++ Linux/Linux/my_header.c
@@ -5,16 +5,20 @@
 unsigned int matching(const char* pattern, FILE *file) {
 	int count = 0;
 	int len = 0;
-	while (!feof(file)) {
-		char buffer[100];
+	while (1) {
+		char buffer[1000];
+		if (fgets(buffer, 1000, file) == NULL) {
+			return count;
+		}
 
-		char *string = fgets(buffer, sizeof(buffer), file);
-
-		if (strstr(buffer,pattern)) 
-		{
+		char* a = strstr(buffer, pattern);
+		while (a != NULL) {
 			count++;
+
+			a = strstr(a+1, pattern);
 		}
 
 	}
+
 	return count;
 }
